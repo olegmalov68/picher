55d4399937ce9961c15f8826ad9efc4d3a3d20cf
olegmalov68 <83117180+olegmalov68@users.noreply.github.com>
2021-05-30 13:41:20 +0300

    Add files via upload

---
+++ pages/schools/js/schools.js
@@ -0,0 +1,30 @@
+document.getElementById('slider_left').onclick = sliderLeft;
+autoSlider();
+var left = 0;
+var timer;
+
+function autoSlider() {
+	timer = setTimeout(sliderLeft, 5000);
+}
+
+function sliderLeft() {
+	var band = document.getElementById('slider_inner');
+	left = left - 1920;
+	if (left < -5750) {
+		left = 0;
+		clearTimeout(timer);
+	}
+	band.style.left = left +'px';
+	autoSlider();
+}
+
+window.onscroll = function showHeader() {
+
+	var header = document.querySelector('#header');
+	if(window.pageYOffset > 200) {
+		header.classList.add('header_fixed');
+	} else {
+		header.classList.remove('header_fixed');
+	}
+
+}
